d149ab37eec2dce0f668e5fcac8c9dc50894296e
oakel <oakel@ucsd.edu>
2019-10-26 15:35:16 -0700

    changed api url

--- src/main/java/garbagerest/GarbageRestController.java
+++ src/main/java/garbagerest/GarbageRestController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 @RestController
 public class GarbageRestController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "/api/add", method = POST)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
