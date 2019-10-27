171e63c8706942808e38ee8300940e5846a9f5ea
oakel <oakel@ucsd.edu>
2019-10-26 17:54:57 -0700

    web controller

--- src/main/java/garbage/GarbageWebController.java
+++ src/main/java/garbage/GarbageWebController.java
@@ -1,12 +1,23 @@
 package garbage;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GarbageWebController {
 
-    @RequestMapping("/")
+
+    @GetMapping("/home")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+
+    @GetMapping("login")
     public String login() {
         return "login";
     }
